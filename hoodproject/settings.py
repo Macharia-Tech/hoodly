f3b4fa49d4350fc1b6760c70e0ddd1454eb47979
AudreyCherrie <mitchelaudrey@gmail.com>
2020-02-26 11:27:47 +0300

    update project

--- hoodproject/settings.py
+++ hoodproject/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hood.apps.HoodConfig',
     'bootstrap4',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hoodproject/urls.py
+++ hoodproject/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url,include
+from django.contrib.auth import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('hoodly.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
+  
 ]
